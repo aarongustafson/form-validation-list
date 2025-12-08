d19a4ba57378e64497f9dce2d94734715a1d664d
Aaron Gustafson <aaron@easy-designs.net>
2025-12-08 09:00:46 -0800

    Harden define

--- define.js
+++ define.js
@@ -1,3 +1,18 @@
 import { FormValidationListElement } from './form-validation-list.js';
 
-customElements.define('form-validation-list', FormValidationListElement);
+export function defineFormValidationList(tagName = 'form-validation-list') {
+	const hasWindow = typeof window !== 'undefined';
+	const registry = hasWindow ? window.customElements : undefined;
+
+	if (!registry || typeof registry.define !== 'function') {
+		return false;
+	}
+
+	if (!registry.get(tagName)) {
+		registry.define(tagName, FormValidationListElement);
+	}
+
+	return true;
+}
+
+defineFormValidationList();
